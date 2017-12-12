34a20ee9400b6b740f9c8be559c7bb6dc1a8f183
hugh <6810110@qq.com>
2017-12-12 09:51:21 +0800

    hugh
    删除信息

--- WebContent/com/sap/me/framework/view/ProcessTypes.js
+++
@@ -1,7 +0,0 @@
-jQuery.sap.declare("com.sap.me.framework.view.ProcessTypes");
-
-com.sap.me.framework.view.ProcessTypes = [
-    {name : "SFC.default.LABEL", value : "SFC"},
-    {name : "SHOP_ORDER.default.LABEL", value : "SHOP_ORDER"},
-    {name : "PROCESS_LOT.default.LABEL", value : "PROCESS_LOT"}
-];
--- WebContent/com/sap/me/production/view/AssyTypes.js
+++
@@ -1,7 +0,0 @@
-jQuery.sap.declare("com.sap.me.production.view.AssyTypes");
-
-com.sap.me.production.view.AssyTypes = [
-    {name : "SFC.default.LABEL", value : "SFC"},
-    {name : "SHOP_ORDER.default.LABEL", value : "SHOP_ORDER"},
-    {name : "PROCESS_LOT.default.LABEL", value : "PROCESS_LOT"}
-];
